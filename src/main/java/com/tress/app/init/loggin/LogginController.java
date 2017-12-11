96a14b40ae1c3ac510a342d945b9a74e42da9031
macbook <sebastian@kometsales.com>
2017-12-11 17:29:23 -0500

    pruebas sin 0Auth

--- src/main/java/com/tress/app/init/loggin/LogginController.java
+++ src/main/java/com/tress/app/init/loggin/LogginController.java
@@ -10,9 +10,12 @@ import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
 import javax.validation.Valid;
+import java.util.HashMap;
+import java.util.Map;
 
 @Controller
 public class LogginController {
@@ -21,10 +24,13 @@ public class LogginController {
     private UserService userService;
 
     @RequestMapping(value={"/", "/login"}, method = RequestMethod.GET)
-    public ModelAndView login(){
-        ModelAndView modelAndView = new ModelAndView();
-        modelAndView.setViewName("login");
-        return modelAndView;
+    @ResponseBody
+    public Map<String, Object> login(){
+        Map<String, Object> result = new HashMap<>();
+
+        result.put("status", 1);
+
+        return result;
     }
 
     @RequestMapping(value="/registration", method = RequestMethod.GET)
@@ -37,8 +43,9 @@ public class LogginController {
     }
 
     @RequestMapping(value = "/registration", method = RequestMethod.POST)
-    public ModelAndView createNewUser(@Valid User user, BindingResult bindingResult) {
-        ModelAndView modelAndView = new ModelAndView();
+    @ResponseBody
+    public Map<String, Object> createNewUser(@Valid User user, BindingResult bindingResult) {
+        Map<String, Object> result = new HashMap<>();
         User userExists = userService.findUserByEmail(user.getEmail());
         if (userExists != null) {
             bindingResult
@@ -46,15 +53,15 @@ public class LogginController {
                             "There is already a user registered with the email provided");
         }
         if (bindingResult.hasErrors()) {
-            modelAndView.setViewName("registration");
+            result.put("status", 0);
         } else {
             userService.saveUser(user);
-            modelAndView.addObject("successMessage", "User has been registered successfully");
-            modelAndView.addObject("user", new User());
-            modelAndView.setViewName("registration");
+            result.put("successMessage", "User has been registered successfully");
+            result.put("user", new User());
+            result.put("status", 1);
 
         }
-        return modelAndView;
+        return result;
     }
 
     @RequestMapping(value="/admin/home", method = RequestMethod.GET)
